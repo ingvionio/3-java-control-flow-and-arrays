4990404982fb8f61fcd8e837db89471cde753ea6
ingvionio <andanteloos74@gmail.com>
2024-09-13 01:03:53 +0500

    task12

--- task12/src/com.example.task12/Task12Main.java
+++ task12/src/com.example.task12/Task12Main.java
@@ -13,6 +13,25 @@ public class Task12Main {
 
     static void selectionSort(int[] arr) {
         //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        if (arr != null && arr.length != 0) {
+            for (int k = 0; k < arr.length - 1; k++) {
+
+
+                int min = Integer.MAX_VALUE;
+                int index = k;
+                for (int i = k; i < arr.length; i++) {
+                    if (arr[i] <= min) {
+                        min = arr[i];
+                        index = i;
+                    }
+                }
+
+                int first = arr[k];
+                arr[k] = min;
+                arr[index] = first;
+
+            }
+        }
     }
 
 }
